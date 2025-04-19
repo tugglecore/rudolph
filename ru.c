e4beb6b53563f60e88489b1d8663de852c778126
tuggle <tuggledemetrius@gmail.com>
2025-04-18 21:39:21 -0400

    add created cell to csv

--- ru.c
+++ ru.c
@@ -4,14 +4,14 @@
 #include <stdbool.h>
 
 typedef struct {
-    int size;
+    int length;
     char contents[];
 } Cell;
 
 typedef struct {
-    int size;
-    Cell cells[];
-} Cells;
+    int amount_of_cells;
+    Cell *cells[];
+} Csv;
 
 int main(void) {
     int BUFFER_SIZE = 1024 * 1024;
@@ -26,6 +26,10 @@ int main(void) {
     }
 
     char delimiters[255] = { [10] = 1, [13] = 1, [44] = 1 };
+    
+    Csv *csv = malloc(sizeof(Csv) + 1000 * sizeof(Cell *));
+    csv->amount_of_cells = 0;
+
 
     while (!feof(fp)) {
         char *buffer = malloc(BUFFER_SIZE);
@@ -43,7 +47,7 @@ int main(void) {
 
         int cursor = 0;
         int count = 0;
-        while (cursor < bytes_read - 1 && count < 10) {
+        while (cursor < bytes_read - 1) {
             int seeker = cursor;
             char token = buffer[seeker];
 
@@ -62,13 +66,22 @@ int main(void) {
                 token_is_not_a_delimiter = delimiters[token];
                 seeker_did_not_exceed_bytes_read = seeker < bytes_read - 1;
             }
+            int cell_length = seeker - cursor + 1;
+            Cell *cell = malloc(sizeof(Cell) + cell_length * sizeof(char));
+            cell->length = cell_length;
 
+            memmove(cell->contents, &buffer[cursor], cell_length);
+
+            csv->cells[csv->amount_of_cells] = cell;
+
+            printf("What is the cell length %d and what is it's contents: [%.*s]\n", cell_length, cell_length, cell->contents);
+            printf("On count %d, Cursor is %d and Seeker is %d and the size is: %d\n", count, cursor, seeker, seeker - cursor);
             cursor = seeker + 1;
-            count++;
-            printf("On count %d, Cursor is %d and Seeker is %d\n", count, cursor, seeker);
         }
 
     }
+    printf("What is the size of a char: %lu\n", sizeof(int *));
+    printf("What is the size of a char: %lu\n", sizeof(Cell *));
 
     return 0;
 }
