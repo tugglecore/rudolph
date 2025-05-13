c9b45ca656b68444a9f3f422c6f23b743b87f90a
tuggle <tuggledemetrius@gmail.com>
2025-05-12 23:13:01 -0400

    improve memory management of csv structure

--- ru.c
+++ ru.c
@@ -1,121 +1,143 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 typedef struct {
-    int length;
-    char contents[];
+  int length;
+  char contents[];
 } Cell;
 
 typedef struct {
-    int amount_of_cells;
-    Cell *cells[];
+  int row_size;
+  int amount_of_cells;
+  char separator;
+  Cell *cells[];
 } Csv;
 
 int main(void) {
-    int allocated_cells = 0;
-    int CELL_BLOCK = 1000;
-    allocated_cells += CELL_BLOCK;
-    int BUFFER_SIZE = 10;
-    // int BUFFER_SIZE = 1024 * 1024;
-    // char filename[] = "users_game_status.csv";
-    char filename[] = "sample.csv";
+  int allocated_cells = 0;
+  int CELL_BLOCK = 1000;
+  allocated_cells += CELL_BLOCK;
 
-    FILE *fp = fopen(filename, "r");
+  int BUFFER_SIZE = 10;
+  // int BUFFER_SIZE = 1024 * 1024;
 
-    if (feof(fp) || ferror(fp)) {
-        printf("An error occurred");
-        exit(1);
+  // char filename[] = "users_game_status.csv";
+  char filename[] = "sample.csv";
+
+  FILE *fp = fopen(filename, "r");
+
+  if (feof(fp) || ferror(fp)) {
+    printf("An error occurred");
+    exit(1);
+  }
+
+  char delimiters[255] = {[10] = 1, [13] = 1, [44] = 1};
+
+  Csv *csv = malloc(sizeof(Csv) + (sizeof(Cell *) * allocated_cells));
+
+  if (csv == NULL) {
+    printf("An error occurred");
+    exit(1);
+  }
+
+  csv->amount_of_cells = 0;
+  csv->separator = ',';
+  csv->row_size = 6;
+
+  int i = 0;
+  while (!feof(fp)) {
+    i++;
+    char *buffer = malloc(BUFFER_SIZE);
+    int bytes_read = fread(buffer, 1, BUFFER_SIZE, fp);
+
+    char *right_fit_buffer = realloc(buffer, bytes_read);
+
+    if (right_fit_buffer == NULL) {
+      printf("Failed to allocate meory");
+      exit(1);
     }
 
-    char delimiters[255] = { [10] = 1, [13] = 1, [44] = 1 };
-    
-    // TODO: check for null pointer
-    Csv *csv = malloc(sizeof(Csv) + allocated_cells * sizeof(Cell *));
-    csv->amount_of_cells = 0;
+    buffer = right_fit_buffer;
 
+    long file_cursor = ftell(fp);
+    printf("What is the current file position: %lu\n", file_cursor);
 
-    int i = 0;
-    while (!feof(fp)) {
-        i++;
-        char *buffer = malloc(BUFFER_SIZE);
-        int bytes_read = fread(buffer, 1, BUFFER_SIZE, fp);
+    int cursor = 0;
+    int iter = 0;
+    printf("How many bytes were read: %d\n", bytes_read);
+    while (cursor < bytes_read && iter < 3) {
+      iter++;
+      int seeker = cursor;
 
-        char *right_fit_buffer = realloc(buffer, bytes_read);
+      while (seeker < bytes_read - 1 && delimiters[buffer[seeker]] == 0)
+        seeker++;
 
-        if (right_fit_buffer == NULL) {
+      printf("On iteration %d, The cursor is %d and the seeker is %d\n", iter,
+             cursor, seeker);
+      if (delimiters[buffer[seeker]] == 1) {
+        if (csv->amount_of_cells == allocated_cells) {
+          allocated_cells += CELL_BLOCK;
+          Csv *right_size_csv =
+              realloc(csv, sizeof(Csv) + (sizeof(Cell *) * allocated_cells));
+
+          if (right_size_csv == NULL) {
             printf("Failed to allocate meory");
             exit(1);
+          }
+
+          csv = right_size_csv;
         }
-        
-        buffer = right_fit_buffer;
 
-        long file_cursor = ftell(fp);
-        printf("What is the current file position: %lu\n", file_cursor);
+        int cell_length = seeker - cursor;
+        Cell *cell = malloc(sizeof(Cell) + (sizeof(char) * cell_length));
+        cell->length = cell_length;
 
-        int cursor = 0;
-        int iter = 0;
-        printf("How many bytes were read: %d\n", bytes_read);
-        while (cursor < bytes_read && iter < 3) {
-            iter++;
-            // printf("What iteration are we on: %d\n", iter);
-            // printf("The cursor is: %d\n", cursor);
-            int seeker = cursor;
+        memmove(cell->contents, &buffer[cursor], cell_length);
 
-            while(seeker < bytes_read - 1 && delimiters[buffer[seeker]] == 0)
-                seeker++;
+        csv->cells[csv->amount_of_cells] = cell;
+        csv->amount_of_cells++;
 
-            printf("On iteration %d, The cursor is %d and the seeker is %d\n", iter, cursor, seeker);
-            if (delimiters[buffer[seeker]] == 1) {
-                // if (csv->amount_of_cells == allocated_cells) {
-                //     allocated_cells += CELL_BLOCK;
-                //     char *right_fit_csv = realloc(csv, sizeof(Csv) + allocated_cells * sizeof(Cell *));
-                //
-                //     if (right_fit_csv == NULL) {
-                //         printf("Failed to allocate meory");
-                //         exit(1);
-                //     }
-                //
-                //     csv = 
-                //
-                // }
-                int cell_length = seeker - cursor + 1;
-                Cell *cell = malloc(sizeof(Cell) + cell_length * sizeof(char));
-                cell->length = cell_length;
-
-                memmove(cell->contents, &buffer[cursor], cell_length);
-
-                // TODO: Realloc csv when amount of cells execeed allocated budget
-                csv->cells[csv->amount_of_cells] = cell;
-                csv->amount_of_cells++;
-
-                // printf("What is the cell length %d and what is it's contents: [%.*s]\n", cell_length, cell_length, cell->contents);
-                // printf("On count %d, Cursor is %d and Seeker is %d and the size is: %d\n", count, cursor, seeker, seeker - cursor);
-                cursor = seeker + 1;
-            }
-
-            if (!feof(fp)) {
-                if (seeker == bytes_read - 1 || cursor == bytes_read) {
-                    printf("Inside the boundary condition, on iteration %d, The cursor is %d and the seeker is %d", iter, cursor, seeker);
-                    long int offset = -1 * (BUFFER_SIZE - cursor);
-                    fseek(fp, offset, SEEK_CUR); 
-                    printf(" And the file position is %lu\n", ftell(fp));
-                    break;
-                }
-            }
-        }
-        if (i == 23) { break; };
+        cursor = seeker + 1;
+      } else {
+        printf("Inside the boundary condition, on iteration %d, The cursor is "
+               "%d and the seeker is %d",
+               iter, cursor, seeker);
+        long int offset = cursor - BUFFER_SIZE;
+        fseek(fp, offset, SEEK_CUR);
+        printf(" And the file position is %lu\n", ftell(fp));
+        break;
+      }
     }
-    // printf("What is the size of a char: %lu\n", sizeof(int *));
-    // printf("What is the size of a char: %lu\n", sizeof(Cell *));
+    if (i == 23) {
+      break;
+    };
+  }
 
-    int cell_count = csv->amount_of_cells;
+  Csv *final_size_csv =
+      realloc(csv, sizeof(Csv) + (sizeof(Cell *) * csv->amount_of_cells));
 
-    for (int i = 0; i < cell_count; i++) {
-        Cell *cell = csv->cells[i];
-        printf("%.*s", cell->length, cell->contents);
+  if (final_size_csv == NULL) {
+    printf("Failed to allocate meory");
+    exit(1);
+  }
+
+  csv = final_size_csv;
+
+  int cell_count = csv->amount_of_cells;
+
+  for (int i = 0; i < cell_count; i++) {
+    Cell *cell = csv->cells[i];
+    // char n = cell->contents;
+    printf("%.*s", cell->length, cell->contents);
+
+    if ((i % csv->row_size) == csv->row_size - 1) {
+        putchar('\n');
+    } else {
+        putchar(csv->separator);
     }
+  }
 
-    return 0;
+  return 0;
 }
