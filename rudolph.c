d7f383eee76b5f45e68255108e34b20469d2c5dc
tuggle <tuggledemetrius@gmail.com>
2025-11-10 23:18:24 -0500

    removed uncessarry files

--- rudolph.c
+++ rudolph.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/sysinfo.h>
+#include <threads.h>
 
 typedef struct {
   int length;
@@ -11,20 +15,40 @@ typedef struct {
 } Cell;
 
 typedef struct {
+  int size;
+  char name[];
+} Heading;
+
+typedef struct {
+  int amount_of_headings;
+  Heading *headings[];
+} Header;
+
+typedef struct {
+  Header *header;
   int row_size;
   int amount_of_cells;
   char separator;
-  Cell * cells[];
+  Cell *cells[];
 } Csv;
+
+typedef struct {
+  thrd_t thread;
+  char *file_contents;
+  int start;
+  int end;
+  char ** output;
+  int output_size;
+} Partition;
+
+int process_csv(void *arg);
 
 int main(void) {
   int CELL_BLOCK = 1024 * 1024;
   int allocated_cells = CELL_BLOCK;
 
-  // int BUFFER_SIZE = 10;
   int BUFFER_SIZE = 1024 * 1024;
 
-  // char filename[] = "users_game_status.csv";
   char filename[] = "sample.csv";
 
   FILE *fp = fopen(filename, "r");
@@ -34,77 +58,186 @@ int main(void) {
     exit(1);
   }
 
-  char delimiters[255] = {[10] = 1, [13] = 1, [44] = 1};
+  int fd = fileno(fp);
 
-  Csv *csv = malloc(sizeof(Csv) + (sizeof(Cell *) * allocated_cells));
+  struct stat *file_attributes = malloc(sizeof(struct stat));
 
-  if (csv == NULL) {
-    printf("An error occurred");
-    exit(1);
+  if (fstat(fd, file_attributes)) {
+    perror("fstat");
+    return 1;
+  }
+  int file_size = file_attributes->st_size;
+
+  char *mapping;
+
+  mapping = mmap(0, file_size, PROT_READ, MAP_SHARED, fd, 0);
+
+  if (mapping == MAP_FAILED) {
+    perror("mmap");
+    return 1;
   }
 
-  csv->amount_of_cells = 0;
-  csv->separator = ',';
-  csv->row_size = 6;
+  // TODO: realloc Headers with > 100 headings
+  Header *header = malloc(sizeof(Header) + 100 * sizeof(Heading *));
+  header->amount_of_headings = 0;
 
-  while (!feof(fp)) {
-    char *buffer = malloc(BUFFER_SIZE);
-    int bytes_read = fread(buffer, 1, BUFFER_SIZE, fp);
+  int line_cursor = 0;
 
-    long file_cursor = ftell(fp);
+  while (line_cursor < file_size) {
+    // TODO: Handle carriage returns
+    // TODO: Handle empty header rows
 
-    int cursor = 0;
-    while (cursor < bytes_read) {
-      int seeker = cursor;
+    int start_of_heading = line_cursor;
+    int heading_cursor = line_cursor;
 
-      while (seeker < bytes_read - 1 && delimiters[buffer[seeker]] == 0)
-        seeker++;
-
-      if (delimiters[buffer[seeker]] == 1) {
-        if (csv->amount_of_cells == allocated_cells) {
-          allocated_cells += CELL_BLOCK;
-          Csv *right_size_csv =
-              realloc(csv, sizeof(Csv) + (sizeof(Cell *) * allocated_cells));
-
-          if (right_size_csv == NULL) {
-            printf("Failed to allocate meory");
-            exit(1);
-          }
-
-          csv = right_size_csv;
-        }
-
-        int cell_length = seeker - cursor;
-        Cell *cell = malloc(sizeof(Cell) + cell_length * sizeof(char));
-        cell->length = cell_length;
-        cell->start = cursor;
-        cell->end = seeker;
-        memmove(&cell->contents, &buffer[cursor], cell_length);
-
-        csv->cells[csv->amount_of_cells] = cell;
-        csv->amount_of_cells++;
-
-        cursor = seeker + 1;
-      } else {
-        long int offset = cursor - BUFFER_SIZE;
-        fseek(fp, offset, SEEK_CUR);
+    do {
+      if (mapping[heading_cursor] == ',' || mapping[heading_cursor] == '\n')
         break;
+    } while (heading_cursor++, heading_cursor < file_size);
+
+    // TODO: Handle empty headings
+    int heading_size = heading_cursor - start_of_heading;
+
+    Heading *heading = malloc(sizeof(Heading) + sizeof(char) * heading_size);
+
+    heading->size = heading_size;
+
+    memcpy(&heading->name, &mapping[start_of_heading], heading_size);
+    header->headings[header->amount_of_headings] = heading;
+    header->amount_of_headings++;
+
+    line_cursor = heading_cursor + 1;
+    if (mapping[heading_cursor] == '\n') {
+      break;
+    }
+  }
+
+  int start_of_data = line_cursor;
+  int remaining_data_in_file = file_size - start_of_data;
+
+  int amount_of_cores = get_nprocs();
+
+  int size_of_partitions = remaining_data_in_file / amount_of_cores;
+
+  Partition * partitions[amount_of_cores];
+  int amount_of_threads_created = 0;
+  int partition_start_cursor = line_cursor;
+
+
+  while (partition_start_cursor < file_size) {
+    Partition *partition = malloc(sizeof(Partition));
+    partition->file_contents = mapping;
+    partition->start = partition_start_cursor;
+    partition->output = malloc(sizeof(char *));
+
+    if (amount_of_threads_created == amount_of_cores - 1) {
+        if (mapping[file_size -1] == '\n') {
+            partition->end = file_size - 2;
+        } else {
+          partition->end = file_size - 1;
+        }
+    } else {
+
+      int estimated_partition_endpoint =
+          partition_start_cursor + size_of_partitions;
+      int partition_end_cursor = estimated_partition_endpoint;
+
+      // TODO: Last partition no matter what; so create thread!
+      if (partition_end_cursor >= file_size) {
+        partition_end_cursor = file_size - 1;
+      }
+
+      // TODO: Handle empty row
+      int alteration = -1;
+      bool partition_end_cursor_went_forward = false;
+
+      while (true) {
+        if (mapping[partition_end_cursor] == '\n')
+          break;
+
+        // end_cursor is at last position in file
+        if (partition_end_cursor + 1 == file_size)
+          break;
+
+        partition_end_cursor += alteration;
+
+        if (partition_end_cursor == partition_start_cursor) {
+          partition_end_cursor = estimated_partition_endpoint;
+          alteration = 1;
+          partition_end_cursor_went_forward = true;
+        }
+      }
+
+      bool end_cursor_at_last_position = partition_end_cursor == file_size - 1;
+      if (end_cursor_at_last_position) {
+          if (partition_end_cursor == '\n') {
+              partition->end = partition_end_cursor - 1;
+          } else {
+        partition->end = partition_end_cursor;
+          }
+      } else {
+        partition->end = partition_end_cursor - 1;
+      }
+
+      partition_start_cursor = partition_end_cursor + 1;
+
+      // int size_of_partitions = remaining_data_in_file / amount_of_cores;
+      if (partition_end_cursor_went_forward && !end_cursor_at_last_position) {
       }
     }
+
+    thrd_t thread;
+
+    thrd_create(&thread, process_csv, partition);
+    partition->thread = thread;
+    partitions[amount_of_threads_created] = partition;
+    amount_of_threads_created++;
+
+    if (amount_of_threads_created == amount_of_cores) 
+        break;
   }
 
-  int cell_count = csv->amount_of_cells;
+  struct stat *stdout_attributes = malloc(sizeof(struct stat));
 
-  for (int i = 0; i < cell_count; i++) {
-    Cell *cell = csv->cells[i];
-    printf("%.*s", cell->length, cell->contents);
+  int a = fileno(stdout);
+  fstat(a, file_attributes);
 
-    if ((i % csv->row_size) == csv->row_size - 1) {
-      putchar('\n');
-    } else {
-      putchar(csv->separator);
-    }
+  for (int i = 0; i < amount_of_threads_created; i++) {
+      int res = 0;
+      Partition *partition = partitions[i];
+      thrd_join(partition->thread, &res);
+      if (res)
+          return -1;
+
+      char * output = *partition->output;
+      int output_size = partition->output_size;
+
+      printf("%.*s", output_size, output);
   }
+
+  return 0;
+}
+
+int process_csv(void *arg) {
+  Partition *partition = (Partition *)arg;
+ 
+  // (end - start) + 1 for inclusive range + 1 for trailing newline
+  char *output = malloc((partition->end - partition->start) + 1 + 1);
+
+  int output_cursor = 0;
+  int input_cursor = partition->start;
+
+  // printf("What was the last character: %d\n", partition->file_contents[partition->end]);
+
+  while (input_cursor <= partition->end) {
+      output[output_cursor] = partition->file_contents[input_cursor];
+      input_cursor++;
+      output_cursor++;
+  }
+
+  output[output_cursor] = '\n';
+  *partition->output = output;
+  partition->output_size = output_cursor + 1;
 
   return 0;
 }
